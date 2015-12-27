c83f88cee0d0f6536bb80ae842d4427c29cd91a8
Hamza Sheikh <hsheikhm@gmail.com>
2015-12-27 21:12:13 +0000

    created a ViewReceiverProfileController to display receiver's profile when a donor taps on "View Profile"

--- changr/AppDelegate.swift
+++ changr/AppDelegate.swift
@@ -152,12 +152,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
     // Directing a user to a specific view controller when they tap on "View Profile" after receiving a local push notification:
     
     func application(application: UIApplication, handleActionWithIdentifier identifier: String?, forLocalNotification notification: UILocalNotification, withResponseInfo responseInfo: [NSObject : AnyObject], completionHandler: () -> Void) {
+        
         if identifier == "VIEW_PROFILE_IDENTIFIER" {
-            print("View profile action")
-            print(notification.userInfo!)
-            
             let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
             let destinationViewController = mainStoryboard.instantiateViewControllerWithIdentifier("ViewReceiverProfileController") as! ViewReceiverProfileController
+            destinationViewController.beaconData = notification.userInfo!["beaconMinor"] as! String
             window!.rootViewController = destinationViewController
             window!.makeKeyAndVisible()
         }
@@ -185,22 +184,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
     }
 
 }
-
-//    func locationManager(manager: CLLocationManager, didDetermineState state: CLRegionState, forRegion region: CLRegion) {
-//
-//        switch state {
-//            case .Inside:
-//                let localNotification:UILocalNotification = UILocalNotification()
-//                localNotification.alertAction = "view options"
-//                localNotification.alertBody = "You are in range of beacons"
-//                localNotification.category = "RECEIVER_IN_RANGE_ALERT"
-//    //          localNotification.userInfo = beaconInfo
-//                localNotification.soundName = UILocalNotificationDefaultSoundName
-//                UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
-//            default: break
-//        }
-//    }
-
-//    func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
-//        enteredRegion = true
-//    }
--- changr/ViewReceiverProfileController.swift
+++ changr/ViewReceiverProfileController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewReceiverProfileController.swift
-//  changr
-//
-//  Created by Hamza Sheikh on 27/12/2015.
-//  Copyright © 2015 Samuel Overloop. All rights reserved.
-//
 
 import UIKit
 
@@ -13,41 +6,52 @@ class ViewReceiverProfileController: UIViewController {
     // MARK: Properties
     
     @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var fullNameDisplay: UILabel!
+    @IBOutlet weak var emailDisplay: UILabel!
+    @IBOutlet weak var dateOfBirthDisplay: UILabel!
+    @IBOutlet weak var genderDisplay: UILabel!
     
     var ref: Firebase!
+    var beaconData: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         ref = Firebase(url: "https://changr.firebaseio.com/users")
+        getReceiverFromDatabaseAndDisplayData()
+    }
+    
+    func getReceiverFromDatabaseAndDisplayData() {
         
-        let hamzaRef = ref.childByAppendingPath("f9357340-0795-4d8e-912e-75a64ae318ad")
+        // Get receiver from database:
         
-            hamzaRef.observeEventType(.Value, withBlock: { snapshot in
-                
-            let decodedData = NSData(base64EncodedString: snapshot.value["profileImage"] as! String, options: NSDataBase64DecodingOptions.IgnoreUnknownCharacters)
-            
-            let decodedImage = UIImage(data: decodedData!)
-            
-            self.profileImageView.image = decodedImage
-                
-            })
-        
+        ref.observeEventType(.Value, withBlock: { snapshot in
+            for item in snapshot.children {
+                let child = item as! FDataSnapshot
+                    if child.value["beaconMinor"] as! String == self.beaconData {
+                        let currentReceiver = child.value
+                        
+                        // Display the receiver's details:
+                        
+                        self.displayReceiverProfileImage((currentReceiver["profileImage"] as? String)!)
+                        self.fullNameDisplay.text = currentReceiver["fullName"] as? String
+                        self.emailDisplay.text = currentReceiver["email"] as? String
+                        self.dateOfBirthDisplay.text = currentReceiver["dateOfBirth"] as? String
+                        self.genderDisplay.text = currentReceiver["gender"] as? String
+                    }
+            }
+        })
+    }
+    
+    // This decodes the base64string into an UIImage:
+    
+    func displayReceiverProfileImage(imageString: String) {
+        let decodedData = NSData(base64EncodedString: imageString, options: NSDataBase64DecodingOptions.IgnoreUnknownCharacters)
+        let decodedImage = UIImage(data: decodedData!)
+        self.profileImageView.image = decodedImage
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
