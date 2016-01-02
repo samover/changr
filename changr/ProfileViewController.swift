4c586fec04b99e6f3713628946a7350f830bfb51
hasulica <raduasandei@gmail.com>
2016-01-02 15:07:27 +0000

    Added accept on payment

--- changr/ProfileViewController.swift
+++ changr/ProfileViewController.swift
@@ -7,27 +7,29 @@
 //
 
 import UIKit
+import accepton
 
-class ProfileViewController: UIViewController {
-
+//This contains the 'buy the watch for $10' page on the Main.storyboard
+class ProfileViewController : UIViewController, AcceptOnViewControllerDelegate {
+    
     let ref = Firebase(url: "https://changr.firebaseio.com/")
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-
+    
     // MARK: Actions
     
     @IBAction func menuButton(sender: AnyObject) {
         let appDelegate:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         appDelegate.centerContainer!.toggleDrawerSide(MMDrawerSide.Left, animated: true, completion: nil)
     }
-
+    
     
     @IBAction func logoutButton(sender: AnyObject) {
         ref.unauth()
@@ -35,7 +37,62 @@ class ProfileViewController: UIViewController {
         
         appDelegate.window?.rootViewController = appDelegate.rootController
         appDelegate.window!.makeKeyAndVisible()
-
+        
     }
 
+    
+    //Segue in progress
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let avc = segue.destinationViewController as? AcceptOnViewController {
+            avc.delegate = self
+            
+            //Name of the item you are selling
+            avc.itemDescription = "My Item Description"
+            
+            //The cost in cents of the item
+            avc.amountInCents = 100
+            
+            //The accessToken
+            avc.accessToken = "pkey_2de6a3b4b90e546a"
+            
+            //Optionally, provide an email to use to auto-fill out the email
+            //field in the credit card form
+            //var userInfo = AcceptOnUIMachineOptionalUserInfo()
+            //userInfo.email = "test@test.com"
+            //avc.userInfo = userInfo
+            
+            //If you're using this in production
+            //avc.isProduction = true
+        }
+    }
+    
+    //User hit the close button, no payment was completed
+    func acceptOnCancelWasClicked(vc: AcceptOnViewController) {
+        //Hide the accept-on UI
+        vc.dismissViewControllerAnimated(true) {
+        }
+    }
+    
+    //Payment did succeed, show a confirmation message
+    func acceptOnPaymentDidSucceed(vc: AcceptOnViewController, withChargeInfo chargeInfo: [String:AnyObject]) {
+        
+        //Dismiss the modal that we showed in the storyboard
+        vc.dismissViewControllerAnimated(true) {
+        }
+        
+        let alertController = UIAlertController(title: "Hurray!", message: "Payment Complete!", preferredStyle: .Alert)
+        
+
+        
+        let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+            // ...
+        }
+        alertController.addAction(OKAction)
+        
+        self.presentViewController(alertController, animated: true) {
+            // ...
+        }
+        
+    }
 }
+
