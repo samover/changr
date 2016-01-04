e360f9c632bde1cd5d1a49178165868d5a0fc6f3
samover <samuel.overloop@gmail.com>
2016-01-04 11:37:04 +0000

    add framworks, remove brdigin header

--- changr-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import <Firebase/Firebase.h>
-#import <MMDrawerController/MMDrawerController.h>
-#import "MMDrawerController+Storyboard.h"
-#import <MMDrawerController/UIViewController+MMDrawerController.h>
-#import <MMDrawerController/MMDrawerBarButtonItem.h>
