26da069fa07532dc4928db26b91d6b1a85e712a3
Hamza Sheikh <hsheikhm@gmail.com>
2016-01-04 13:01:11 +0000

    integrated paypal

--- changr/AppDelegate.swift
+++ changr/AppDelegate.swift
@@ -38,6 +38,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
         locationManager.requestAlwaysAuthorization()
         locationManager.delegate = self
         
+        // For PayPal Integration:
+            
+        PayPalMobile.initializeWithClientIdsForEnvironments([PayPalEnvironmentProduction: "AaAqHxwwgSS6LjhixZVQqFZNeCrjzaDgwIdSBvOoxptrcGAmDTvtrNrH4500aR7o9b42WUe-hTVq62hA", PayPalEnvironmentSandbox: "AUOe5U1jc9EbPxihVXMPlFVbsqcZwAan42In7Rue5QZZsAL3H3U9uSlQdBSKUyznjRUEBdAxTDGY_7KH"])
+        
         // Building the Push Notification Options:
 
         let notificationActionViewProfile :UIMutableUserNotificationAction = UIMutableUserNotificationAction()
---
+++ changr/ZZAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  ZZAppDelegate.h
+//  changr
+//
+//  Created by Hamza Sheikh on 04/01/2016.
+//  Copyright © 2016 Samuel Overloop. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface ZZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property(strong, nonatomic) UIWindow *window;
+
+@end
