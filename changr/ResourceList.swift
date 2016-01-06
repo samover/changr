21667e56f339a74072ff6bb74841b64d06307a33
hasulica <raduasandei@gmail.com>
2016-01-06 15:45:18 +0000

    Homepage

---
+++ changr/ResourceList.swift
@@ -0,0 +1,16 @@
+//
+//  ResourceList.swift
+//  changr
+//
+//  Created by Radu Asandei on 05/01/2016.
+//  Copyright © 2016 Samuel Overloop. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Resource {
+    var name: String
+    var link: String
+    var image: UIImage
+}
--- changr/ViewController.swift
+++ changr/ViewController.swift
@@ -11,25 +11,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     
     // MARK: Properties
     var appDelegate: AppDelegate!
     let firebase = FirebaseWrapper()
+    let homelessNews = ["The Guardian", "The Big Issue", "Crisis.ORG"]
+    let ResourceCellIdentifier = "ResourceCell"
+    
+    struct Resource {
+        var name: String
+        var link: String
+        var image: String
+    }
+    
+    let ResourceList = [
+        Resource(name: "Crisis UK", link: "http://www.crisis.org.uk/", image: "crisis-logo"),
+        Resource(name: "Shelter UK", link: "http://www.shelter.org.uk/", image: "shelter-logo"),
+        Resource(name: "Homeless UK", link: "http://www.crisis.org.uk/", image: "homeless-logo"),
+        Resource(name: "Shelter From The Storm", link: "http://www.sfts.org.uk/", image: "storm-logo")
+    ]
+    
     
     // MARK: Outlets
-    @IBOutlet weak var currentUserLabel: UILabel!
+
     
     // MARK: UIViewController Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ResourceList.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(ResourceCellIdentifier, forIndexPath: indexPath) as UITableViewCell
+        let row = indexPath.row
+        cell.textLabel?.text = ResourceList[row].name
+        cell.imageView?.image = UIImage(named: ResourceList[row].image)
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let row = indexPath.row
+        UIApplication.sharedApplication().openURL(NSURL(string:ResourceList[row].link)!)
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
     
     // MARK: Actions
     @IBAction func menuButton(sender: AnyObject) {
