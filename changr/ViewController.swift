54c2d9c3f94d3c2819c6997b879baaa938b17fdc
Hamza Sheikh <hsheikhm@gmail.com>
2016-01-06 21:34:30 +0000

    added a title and description label with some animations

--- changr/ViewController.swift
+++ changr/ViewController.swift
@@ -17,13 +17,38 @@ class ViewController: UIViewController {
     var appDelegate: AppDelegate!
     let firebase = FirebaseWrapper()
     
-    // MARK: Outlets
-    @IBOutlet weak var currentUserLabel: UILabel!
+    var welcomeLabel: UILabel!
+    var descriptionLabel: UILabel!
     
     // MARK: UIViewController Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    
+        welcomeLabel = UILabel()
+        welcomeLabel.text = "Welcome to Changr"
+        welcomeLabel.font = UIFont.systemFontOfSize(25)
+        welcomeLabel.sizeToFit()
+        welcomeLabel.center = CGPoint(x: 100, y: 40)
+        view.addSubview(welcomeLabel)
+        
+        UIView.animateWithDuration(0.5, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.0, options: UIViewAnimationOptions.TransitionNone, animations: {
+            self.welcomeLabel.center = CGPoint(x: 100, y:40 + 200)
+            }, completion: nil)
+        
+        descriptionLabel = UILabel()
+        descriptionLabel.text = "Changr is an app to help those who are homeless..."
+        descriptionLabel.font = UIFont.boldSystemFontOfSize(15)
+        descriptionLabel.sizeToFit()
+        descriptionLabel.center = CGPoint(x: 200, y: 90)
+        view.addSubview(descriptionLabel)
+        
+        descriptionLabel.alpha = 0
+        
+        UIView.animateWithDuration(2.0, delay: 0.5, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.0, options: UIViewAnimationOptions.TransitionNone, animations: {
+            self.descriptionLabel.center = CGPoint(x: 200, y: 90 + 200)
+            self.descriptionLabel.alpha = 1
+            }, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
