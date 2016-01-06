3183297dd808e08d6880d242487559a96570b14a
Fergus Lemon <fergus.lemon@gmail.com>
2016-01-06 12:48:51 +0000

    new profile side bar view

--- changr/ProfileViewController.swift
+++ changr/ProfileViewController.swift
@@ -7,17 +7,34 @@
 //
 
 import UIKit
+import Firebase
 
 class ProfileViewController: UIViewController {
 
     // MARK: Properties
     let firebase = FirebaseWrapper()
     var appDelegate: AppDelegate!
-    
+    var ref: Firebase!
+//    
+//    @IBOutlet weak var profileImage: UIImageView!
+//    @IBOutlet weak var fullNameLabel: UILabel!
+//    @IBOutlet weak var emailLabel: UILabel!
+//    @IBOutlet weak var dateOfBirthLabel: UILabel!
+//    @IBOutlet weak var genderLabel: UILabel!
+
+    // CONNECT TO FIREBASE DB
+
+
+
+
+
     // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+
+        let usersRef = firebase.ref.childByAppendingPath("/users")
+        print(usersRef.authData)
     }
 
     override func didReceiveMemoryWarning() {
