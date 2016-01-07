6b46e3c8395d5489f4b31c8013845e8e44acdda3
Hamza Sheikh <hsheikhm@gmail.com>
2016-01-07 00:30:59 +0000

    completed description text for description label

--- changr/ViewController.swift
+++ changr/ViewController.swift
@@ -28,19 +28,19 @@ class ViewController: UIViewController {
         // Welcome Label:
         welcomeLabel = UILabel(frame: CGRectMake(0, 0, 350, 350))
         welcomeLabel.text = "Welcome to Changr!"
-        welcomeLabel.font = UIFont.boldSystemFontOfSize(27)
-        welcomeLabel.center = CGPointMake(100, 40)
+        welcomeLabel.font = UIFont.boldSystemFontOfSize(28)
+        welcomeLabel.center = CGPointMake(100, 2)
         welcomeLabel.textAlignment = NSTextAlignment.Center
         view.addSubview(welcomeLabel)
         
-        UIView.animateWithDuration(0.5, delay: 0.0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.0, options: UIViewAnimationOptions.TransitionNone, animations: {
-            self.welcomeLabel.center = CGPointMake(160, 240)
+        UIView.animateWithDuration(0.5, delay: 0.0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+            self.welcomeLabel.center = CGPointMake(160, 270)
             }, completion: nil)
         
         // Description Label:
         descriptionLabel = UILabel(frame: CGRectMake(0, 0, 270, 450))
-        descriptionLabel.text = "Changr is an app to help those who are homeless..."
-        descriptionLabel.font = UIFont.italicSystemFontOfSize(18)
+        descriptionLabel.text = "...This app was built by a team of Makers Academy students. It is to help people who are homeless in receiving donations from the general public. The app works with Estimote beacons so that donors can receive a custom notification on their phone when walking past a registered homeless person..."
+        descriptionLabel.font = UIFont.italicSystemFontOfSize(17)
         descriptionLabel.center = CGPointMake(200, 90)
         descriptionLabel.textAlignment = NSTextAlignment.Center
         descriptionLabel.numberOfLines = 0
@@ -49,7 +49,7 @@ class ViewController: UIViewController {
         descriptionLabel.alpha = 0
         
         UIView.animateWithDuration(2.0, delay: 0.5, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.0, options: UIViewAnimationOptions.TransitionNone, animations: {
-            self.descriptionLabel.center = CGPointMake(160, 290)
+            self.descriptionLabel.center = CGPointMake(160, 400)
             self.descriptionLabel.alpha = 1
             }, completion: nil)
     }
