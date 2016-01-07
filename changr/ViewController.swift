9b5773e511296e47d2a4068482d598b493e2e3ef
Hamza Sheikh <hsheikhm@gmail.com>
2016-01-07 01:12:39 +0000

    added a custom background color for description label

--- changr/ViewController.swift
+++ changr/ViewController.swift
@@ -39,9 +39,10 @@ class ViewController: UIViewController {
             }, completion: nil)
         
         // Description Label:
-        descriptionLabel = UILabel(frame: CGRectMake(0, 0, 270, 450))
+        descriptionLabel = UILabel(frame: CGRectMake(0, 0, 255, 230))
         descriptionLabel.text = "...This app was built by a team of Makers Academy students. It is to help people who are homeless in receiving donations from the general public. The app works with Estimote beacons so that donors can receive a custom notification on their phone when walking past a registered homeless person..."
         descriptionLabel.font = UIFont.italicSystemFontOfSize(17)
+        descriptionLabel.backgroundColor = UIColor(red: 0/255.0, green: 157/255.0, blue: 139/255.0, alpha: 1)
         descriptionLabel.center = CGPointMake(200, 90)
         descriptionLabel.textAlignment = NSTextAlignment.Center
         descriptionLabel.numberOfLines = 0
@@ -50,7 +51,7 @@ class ViewController: UIViewController {
         descriptionLabel.alpha = 0
         
         UIView.animateWithDuration(2.0, delay: 0.5, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.0, options: UIViewAnimationOptions.TransitionNone, animations: {
-            self.descriptionLabel.center = CGPointMake(160, 400)
+            self.descriptionLabel.center = CGPointMake(160, 410)
             self.descriptionLabel.alpha = 1
             }, completion: nil)
     }
